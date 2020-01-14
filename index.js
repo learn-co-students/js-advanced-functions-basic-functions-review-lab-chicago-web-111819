919c52b6118266bb89ee521554a07eb2b79a9387
dut-a <d.dut@live.com>
2020-01-14 12:40:31 -0600

    Done.

--- index.js
+++ index.js
@@ -8,13 +8,35 @@ function mondayWork(activity = "go to the office") {
 }
 
 // "Function-level Scope"
-function outer(greeting, msg="It's a fine day to learn") {
-  return function(name, lang="Python") {
+function outer(greeting, msg = "It's a fine day to learn") {
+  return function(name, lang = "Python") {
     return `${greeting}, ${name}! ${msg} ${lang}`
   }
 }
  
 outer("Hello")("student", "JavaScript");
+
+
+function wrapAdjective(nicety = "*") {
+  return function(adjective = "special") {
+    return `You are ${nicety}${adjective}${nicety}!`;
+  };
+}
+
+let Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b
+};
+
+function actionApplyer(startingNum, actions) {
+  let resultingNum = startingNum;
+  for(let i = 0; i < actions.length; i++) {
+    resultingNum = actions[i](resultingNum);
+  }
+  return resultingNum;
+}
 
 
 
