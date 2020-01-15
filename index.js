3f0045e4ef4ad4126abc1c2db772a34a68491cd7
Alex Bonwit <alexbonwit@mac.com>
2020-01-15 11:08:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(adjective="special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2
+    },
+    divide: function(num1, num2) {
+        return num1 / num2
+    }
+
+}
+
+function actionApplyer(start, functions) {
+    
+    for (let i = 0; i < functions.length; i++) {
+        start = functions[i](start)
+    }
+    return start
+}
